df3213358144fc15a6f8b394c0328640818e0cba
Vdovin Anton <antonvdovin@outlook.com>
2016-05-21 23:35:45 +0300

    Sample work with messages

--- SkypeWatcher/SkypeWatcher/Startup.cs
+++ SkypeWatcher/SkypeWatcher/Startup.cs
@@ -2,16 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using SKYPE4COMLib;
 
 namespace SkypeWatcher
 {
     public class Startup
     {
+        private static readonly Skype Skype = new Skype();
         private static void Main(string[] args)
         {
-            Console.WriteLine("Run");
-            Console.ReadKey();
+            Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    Skype.MessageStatus += OnMessageRecived;
+                    Skype.CallDtmfReceived += OnCallRecived;
+                    Skype.Attach();
+                    Console.WriteLine("Skype attached");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error\n{e.Message}");
+                    throw;
+                }
+                while (true)
+                {
+                    Thread.Sleep(1000);
+                }
+            }, TaskCreationOptions.LongRunning);
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        private static void OnCallRecived(Call pcall, string code)
+        {
+            Console.WriteLine("Call is recived");
+        }
+
+        private static void OnMessageRecived(ChatMessage pmessage, TChatMessageStatus status)
+        {
+            if (status == TChatMessageStatus.cmsReceived)
+            {
+                Console.WriteLine($"Message:\n{pmessage.Body}");
+            }
         }
     }
 }
